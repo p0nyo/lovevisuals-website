0a0c34d88e0f387a6d6f0cd1179fa6b396faf713
treyson tsen <78147374+p0nyo@users.noreply.github.com>
2026-04-20 22:20:52 +1200

    refactor: convert cardimage calls in pages to use new component

--- app/(website)/about/page.tsx
+++ app/(website)/about/page.tsx
@@ -1,13 +1,12 @@
 import CardImage from "@/components/home/CardImage"
 
 
-
 export default function About() {
     return (
             <div className="bg-[linear-gradient(to_bottom,#678BAA,#EBE8D8)] relative min-h-screen w-full flex flex-col items-center justify-center gap-8 bg-gray-500">
                 <div className="flex flex-row w-full items-center justify-start gap-4 px-60">
                     <div className="w-full">
-                        <CardImage src="/img.jpg" alt="" width={500} height={430} />
+                        <CardImage src="/img.jpg" alt="" className="w-40 h-60 md:w-126 md:h-116" />
                     </div>
                     <div className="flex flex-col">
                         <div className="flex items-center justify-center h-full whitespace-nowrap text-9xl">
@@ -19,10 +18,10 @@ export default function About() {
                     </div>
                 </div>
                 <div className="flex flex-row gap-4">
-                    <CardImage src="/img.jpg" alt="" width={400} height={300} />
-                    <CardImage src="/img.jpg" alt="" width={400} height={300} />
-                    <CardImage src="/img.jpg" alt="" width={400} height={300} />
-                    <CardImage src="/img.jpg" alt="" width={400} height={300} />
+                    <CardImage src="/img.jpg" alt="" className="w-40 h-60 md:w-100 md:h-76" />
+                    <CardImage src="/img.jpg" alt="" className="w-40 h-60 md:w-100 md:h-76" />
+                    <CardImage src="/img.jpg" alt="" className="w-40 h-60 md:w-100 md:h-76" />
+                    <CardImage src="/img.jpg" alt="" className="w-40 h-60 md:w-100 md:h-76" />
                 </div>
             </div>
     );
--- app/(website)/portfolio/page.tsx
+++ app/(website)/portfolio/page.tsx
@@ -6,7 +6,7 @@ export default function Portfolio() {
     return (
             <div className="bg-[linear-gradient(to_bottom,#678BAA,#EBE8D8)] relative min-h-screen w-full flex flex-row items-center justify-center gap-8 bg-gray-500">
                 <div className="relative">
-                    <CardImage src="/img.jpg" alt="" width={600} height={800} />
+                    <CardImage src="/img.jpg" alt="" className="w-40 h-60 md:w-140 md:h-200"/>
                     <div>
                         <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-8xl">
                             images
@@ -14,7 +14,7 @@ export default function Portfolio() {
                     </div>
                 </div>
                 <div className="relative">
-                    <CardImage src="/img.jpg" alt="" width={600} height={800} />
+                    <CardImage src="/img.jpg" alt="" className="w-40 h-60 md:w-140 md:h-200"/>
                     <div>
                         <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-8xl">
                             videos
