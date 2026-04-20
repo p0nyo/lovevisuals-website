d3cbeb2b194de42809ddd8a461cecd2002f35169
treyson tsen <78147374+p0nyo@users.noreply.github.com>
2026-04-20 22:53:57 +1200

    feat: add global button component

---
+++ components/ui/GlobalButton.tsx
@@ -0,0 +1,33 @@
+import clsx from "clsx";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    variant?: "primary" | "secondary" | "ghost";
+    size?: "sm" | "md" | "lg";
+}
+
+export default function GlobalButton({
+    variant = "primary",
+    size = "md",
+    className,
+    ...props
+}: ButtonProps) {
+    return (
+        <button
+        className={clsx(
+            "cursor-pointer inline-flex items-center justify-center rounded-sm transition disabled:opacity-50",
+            {
+            "bg-[#678BAA] text-[#EBE8D8]": variant === "primary",
+            "bg-[#EBE8D8] text-[#678BAA]": variant === "secondary",
+            "bg-transparent hover:bg-gray-100": variant === "ghost",
+            },
+            {
+            "px-4 py-2 text-sm": size === "sm",
+            "px-6 py-3 text-lg font-black": size === "md",
+            "px-8 py-4 text-xl": size === "lg",
+            },
+            className
+        )}
+        {...props}
+        />
+    );
+}
